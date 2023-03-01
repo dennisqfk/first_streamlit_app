94b280afb2394a68472fb34f88eda6ee6602e45d
dennisqfk <54928478+dennisqfk@users.noreply.github.com>
2023-03-01 21:08:37 +0700

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text🥣('Omega 3 & Blueberry Oatmeal')
-streamlit.text🥗('Kale, Spinach & Rocket Smoothie')
-streamlit.text🐔('Hard-Boiled Free-Range Egg')
-streamlit.text🥑🍞('Avocado Toast')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
