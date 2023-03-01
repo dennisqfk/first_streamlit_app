131ac77b925ac49b97982727ae590cf6b92f8b62
dennisqfk <54928478+dennisqfk@users.noreply.github.com>
2023-03-01 21:06:51 +0700

    Update streamlit_app.py
    added icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header🥑🍞 ('Breakfast Menu')
+streamlit.text🥣('Omega 3 & Blueberry Oatmeal')
+streamlit.text🥗('Kale, Spinach & Rocket Smoothie')
+streamlit.text🐔('Hard-Boiled Free-Range Egg')
